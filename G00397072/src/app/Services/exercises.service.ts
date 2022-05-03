640d8c5cf92f4293df380696c0ebcd42db4d2555
Eoghan-Barker <g00397072@gmit.ie>
2022-05-03 18:05:04 +0100

    basic api requests

---
+++ G00397072/src/app/Services/exercises.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExercisesService {
+
+  constructor(private httpClient:HttpClient) { }
+
+  GetArmsData():Observable<any>{
+    return this.httpClient.get('https://wger.de/api/v2/exercise/?language=2&category=8&limit=99');
+  }
+
+  GetShouldersData():Observable<any>{
+    return this.httpClient.get('https://wger.de/api/v2/exercise/?language=2&category=13&limit=99');
+  }
+
+  GetChestData():Observable<any>{
+    return this.httpClient.get('https://wger.de/api/v2/exercise/?language=2&category=11&limit=99');
+  }
+
+  GetLegsData():Observable<any>{
+    return this.httpClient.get('https://wger.de/api/v2/exercise/?language=2&category=9&limit=99');
+  }
+
+  GetAbsData():Observable<any>{
+    return this.httpClient.get('https://wger.de/api/v2/exercise/?language=2&category=10&limit=99');
+  }
+
+  GetCalvesData():Observable<any>{
+    return this.httpClient.get('https://wger.de/api/v2/exercise/?language=2&category=14&limit=99');
+  }
+}
--- G00397072/src/app/tab1/tab1.page.ts
+++ G00397072/src/app/tab1/tab1.page.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ExercisesService} from  '../Services/exercises.service'
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
+  ArmsData:any = [];
+  ShoulderData:any = [];
+  Exercise = new Array<string>(5);
+  Sets = new Array<number>(5);
+  Reps = new Array<number>(5);
+  Weight = new Array<number>(5);
 
-  constructor() {}
 
+  constructor(private exerciseService:ExercisesService) {}
+
+  ngOnInit(){
+    this.exerciseService.GetArmsData().subscribe(
+      (data)=>{
+        this.ArmsData = data.results; 
+        console.log(this.ArmsData);
+      }
+    );
+
+    this.exerciseService.GetShouldersData().subscribe(
+      (data)=>{
+        this.ShoulderData = data.results; 
+        console.log(this.ShoulderData);
+      }
+    );
+
+
+  }
 }
